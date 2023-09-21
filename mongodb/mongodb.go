cd4ff8ba8f6d3e6d16fec223c6265366428dc814
Supakorn Korpraphan <supakorn.korpraphan@gmail.com>
2023-09-21 17:27:57 +0700

    Add statements to ping before finishing connect function

--- mongodb/mongodb.go
+++ mongodb/mongodb.go
@@ -2,6 +2,7 @@ package mongodb
 
 import (
 	"context"
+	"time"
 
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
@@ -16,7 +17,12 @@ type MongoDBConn struct {
 
 func (db *MongoDBConn) Connect() error {
 
-	client, err := mongo.Connect(context.TODO(), db.opts)
+	client, err := mongo.Connect(context.Background(), db.opts)
+	if err != nil {
+		return err
+	}
+
+	err = client.Ping(context.Background(), nil)
 	if err != nil {
 		return err
 	}
@@ -27,7 +33,7 @@ func (db *MongoDBConn) Connect() error {
 }
 
 func (db *MongoDBConn) Disconnect() error {
-	return db.client.Disconnect(context.TODO())
+	return db.client.Disconnect(context.Background())
 }
 
 func (db *MongoDBConn) GetDatabase() *mongo.Database {
@@ -42,7 +48,10 @@ func (db *MongoDBConn) GetCollection(collectionName string) *mongo.Collection {
 func New(uri string, dbName string) MongoDBConn {
 
 	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
-	opts := options.Client().ApplyURI(uri).SetServerAPIOptions(serverAPI)
+	opts := options.Client()
+	opts.ApplyURI(uri)
+	opts.SetServerAPIOptions(serverAPI)
+	opts.SetTimeout(3 * time.Second)
 
 	return MongoDBConn{
 		opts:         opts,
