f3c6cc1f87632983dfb94cb31f23edb4678a37f0
Supakorn Korpraphan <supakorn.korpraphan@gmail.com>
2023-10-06 16:13:38 +0700

    Create users model

---
+++ models/users/users.go
@@ -0,0 +1,292 @@
+package users
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"slices"
+	"strings"
+
+	serverError "github.com/supakorn-kn/go-crud/errors"
+	"github.com/supakorn-kn/go-crud/models"
+	"github.com/supakorn-kn/go-crud/mongodb"
+	"github.com/supakorn-kn/go-crud/objects"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type SearchOption struct {
+	CurrentPage int                `json:"current_page"`
+	UserID      string             `json:"user_id,omitempty"`
+	Username    models.MatchOption `json:"username,omitempty"`
+	AccountName models.MatchOption `json:"account_name,omitempty"`
+	Email       models.MatchOption `json:"email,omitempty"`
+}
+
+type UsersModel struct {
+	models.BaseModel[objects.User]
+}
+
+const userIDIndex = "user_id_1"
+const accountNameIndex = "account_name_1"
+
+func NewUsersModel(conn *mongodb.MongoDBConn, paginateSize ...int) (*UsersModel, error) {
+
+	var searchSize int = 10
+	var paginateSizeLen = len(paginateSize)
+	if paginateSizeLen > 1 {
+		return nil, errors.New("PaginateSize can have only one elements")
+	} else if paginateSizeLen == 1 {
+		searchSize = paginateSize[0]
+	}
+
+	var model = new(UsersModel)
+
+	coll, err := model.createCollection(conn)
+	if err != nil {
+		return nil, err
+	}
+
+	err = model.createIndexes(coll)
+	if err != nil {
+		return nil, err
+	}
+
+	err = model.Inject(coll, searchSize, "user_id")
+	if err != nil {
+		return nil, err
+	}
+
+	return model, nil
+}
+
+func (UsersModel) GetCollectionName() string {
+	return "users"
+}
+
+func (m UsersModel) createCollection(conn *mongodb.MongoDBConn) (*mongo.Collection, error) {
+
+	crudDB := conn.GetDatabase()
+	collectionName := m.GetCollectionName()
+
+	collectionNameList, err := crudDB.ListCollectionNames(context.Background(), bson.D{})
+	if err != nil {
+		return nil, err
+	}
+
+	validator := bson.D{
+		{
+			Key: "$jsonSchema", Value: bson.M{
+				"bsonType": "object",
+				"required": []string{"user_id", "username", "password", "account_name", "email"},
+				"properties": bson.M{
+					"user_id": bson.M{
+						"bsonType":    "string",
+						"minimum":     1,
+						"description": "User ID must not be empty",
+					},
+					"username": bson.M{
+						"bsonType":    "string",
+						"description": "Username must not be empty",
+					},
+					"password": bson.M{
+						"bsonType":    "string",
+						"description": "Password must not be empty",
+					},
+					"account_name": bson.M{
+						"bsonType":    "string",
+						"minimum":     1,
+						"description": "Account name must not be empty",
+					},
+					"email": bson.M{
+						"bsonType":    "string",
+						"description": "Email must not be empty",
+					},
+				},
+			},
+		},
+	}
+
+	if slices.Contains(collectionNameList, collectionName) {
+
+		cmd := bson.D{
+			{Key: "collMod", Value: collectionName},
+			{Key: "validator", Value: validator},
+			{Key: "validationLevel", Value: "strict"},
+		}
+
+		option := options.RunCmd()
+		result := crudDB.RunCommand(context.Background(), cmd, option)
+		if err := result.Err(); err != nil {
+			return nil, err
+		}
+
+		return conn.GetCollection(collectionName), nil
+	}
+
+	collectionOption := options.CreateCollection()
+	collectionOption.SetValidator(validator)
+	collectionOption.SetValidationLevel("strict")
+
+	err = crudDB.CreateCollection(context.Background(), collectionName, collectionOption)
+	if err != nil {
+		return nil, err
+	}
+
+	return conn.GetCollection(collectionName), nil
+}
+
+func (m UsersModel) createIndexes(coll *mongo.Collection) error {
+
+	cur, err := coll.Indexes().List(context.Background())
+	if err != nil {
+		return err
+	}
+
+	var indexes []bson.M
+	err = cur.All(context.Background(), &indexes)
+	if err != nil {
+		return err
+	}
+
+	contains := slices.ContainsFunc(indexes, func(m primitive.M) bool {
+		return m["name"] == userIDIndex
+	})
+
+	if !contains {
+
+		indexModelOption := options.Index().SetName(userIDIndex).SetUnique(true)
+		indexModel := mongo.IndexModel{
+			Keys: bson.D{
+				{Key: "user_id", Value: 1},
+			},
+			Options: indexModelOption,
+		}
+
+		_, err = coll.Indexes().CreateOne(context.Background(), indexModel)
+		if err != nil {
+			return err
+		}
+	}
+
+	contains = slices.ContainsFunc(indexes, func(m primitive.M) bool {
+		return m["name"] == accountNameIndex
+	})
+
+	if !contains {
+
+		indexModelOption := options.Index().SetName(accountNameIndex).SetUnique(true)
+		indexModel := mongo.IndexModel{
+			Keys: bson.D{
+				{Key: "account_name", Value: 1},
+			},
+			Options: indexModelOption,
+		}
+
+		_, err = coll.Indexes().CreateOne(context.Background(), indexModel)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (m UsersModel) Insert(user objects.User) error {
+
+	if strings.EqualFold(user.UserID, "") ||
+		strings.EqualFold(user.Username, "") ||
+		strings.EqualFold(user.Password, "") ||
+		strings.EqualFold(user.AccountName, "") ||
+		strings.EqualFold(user.Email, "") {
+
+		//TODO: Add validation failed
+		return errors.New(";w;")
+	}
+
+	filter := bson.D{
+		{
+			Key: "$or", Value: bson.A{
+				bson.D{{Key: m.ItemIDKey, Value: user.UserID}},
+				bson.D{{Key: "username", Value: user.Username}},
+				bson.D{{Key: "account_name", Value: user.AccountName}},
+				bson.D{{Key: "email", Value: user.Email}},
+			},
+		},
+	}
+
+	err := m.Coll.FindOne(context.Background(), filter).Err()
+	if err == nil {
+		return serverError.DataAlreadyInUsedError.New()
+	}
+
+	if !errors.Is(err, mongo.ErrNoDocuments) {
+		return err
+	}
+
+	return m.BaseModel.Insert(user)
+}
+
+func (m UsersModel) Search(opt SearchOption) (paginationResult models.PaginationData[objects.User], paginationErr error) {
+
+	var builder = models.NewSearchPipelineBuilder()
+	builder.Skip((opt.CurrentPage - 1) * m.BaseModel.SearchLenLimit)
+	builder.Limit(m.BaseModel.SearchLenLimit)
+	builder.SortedBy([]models.SortData{
+		{
+			Key:    m.ItemIDKey,
+			SortBy: models.SortASC,
+		},
+	})
+
+	if !strings.EqualFold(opt.UserID, "") {
+
+		fmt.Println("Set user_id")
+
+		paginationErr = builder.Match("user_id", opt.UserID, models.EqualMatchType)
+		if paginationErr != nil {
+			return
+		}
+	}
+
+	if !opt.AccountName.IsNil() {
+
+		fmt.Println("Set account_name")
+
+		paginationErr = builder.Match("account_name", opt.AccountName.Value, opt.AccountName.MatchType)
+		if paginationErr != nil {
+			return
+		}
+	}
+
+	if !opt.Username.IsNil() {
+
+		fmt.Println("Set username")
+
+		paginationErr = builder.Match("username", opt.Username.Value, opt.Username.MatchType)
+		if paginationErr != nil {
+			return
+		}
+	}
+
+	if !opt.Email.IsNil() {
+
+		fmt.Println("Set email")
+
+		paginationErr = builder.Match("email", opt.Email.Value, opt.Email.MatchType)
+		if paginationErr != nil {
+			return
+		}
+	}
+
+	pipeline := builder.BuildPipeline()
+
+	paginationResult, paginationErr = m.BaseModel.Search(models.BaseSearchOption{
+		CurrentPage: opt.CurrentPage,
+		Pipeline:    pipeline,
+	})
+
+	return
+}
---
+++ models/users/users_test.go
@@ -0,0 +1,554 @@
+package users
+
+import (
+	"context"
+	"testing"
+
+	"github.com/brianvoe/gofakeit/v6"
+	"github.com/stretchr/testify/suite"
+	"github.com/supakorn-kn/go-crud/mongodb"
+	"github.com/supakorn-kn/go-crud/objects"
+	"go.mongodb.org/mongo-driver/bson"
+)
+
+type UsersModelTestSuite struct {
+	suite.Suite
+	conn         *mongodb.MongoDBConn
+	model        *UsersModel
+	insertedUser objects.User
+}
+
+func (s *UsersModelTestSuite) SetupSuite() {
+
+	//TODO: Will set parameter from env
+	conn := mongodb.New("mongodb://localhost:27017", "go-crud_test")
+	err := conn.Connect()
+	if err != nil {
+		s.FailNow("Create Mongodb connection failed", err)
+	}
+
+	newModel, err := NewUsersModel(&conn)
+	if err != nil {
+		conn.Disconnect()
+		s.FailNow("Setup User model failed", err)
+	}
+
+	s.model = newModel
+	s.conn = &conn
+}
+
+func (s *UsersModelTestSuite) BeforeTest(suiteName, testName string) {
+
+	if testName == "TestInsert" || testName == "TestSearch" {
+		return
+	}
+
+	s.insertedUser = fakeUser()
+	s.Require().NoError(s.model.Insert(s.insertedUser), "Setup test failed from inserting users")
+}
+
+func (s *UsersModelTestSuite) AfterTest(suiteName, testName string) {
+
+	_, err := s.model.Coll.DeleteMany(context.Background(), bson.D{})
+	s.Require().NoError(err)
+}
+
+func (s *UsersModelTestSuite) TearDownSuite() {
+
+	s.conn.GetDatabase().Drop(context.Background())
+	s.conn.Disconnect()
+}
+
+func (s *UsersModelTestSuite) TestInsert() {
+
+	s.Run("Should insert valid user properly", func() {
+
+		user := fakeUser()
+		s.Require().NoError(s.model.Insert(user), "Inserting User failed")
+
+		result := s.model.Coll.FindOne(context.Background(), bson.D{{Key: "user_id", Value: user.UserID}})
+
+		var actual objects.User
+		s.Require().NoError(result.Decode(&actual), "Unmarshalling inserted User failed")
+		s.Require().EqualValues(user, actual, "Read data is not the same as inserted")
+	})
+
+	s.Run("Should throw error when insert user with existed data", func() {
+
+		user := fakeUser()
+		s.Require().NoError(s.model.Insert(user), "Inserting User failed")
+
+		s.Run("Existed user_id", func() {
+
+			newUser := fakeUser()
+			newUser.UserID = user.UserID
+			s.Require().Error(s.model.Insert(newUser), "Should have thrown error")
+		})
+
+		s.Run("Existed username", func() {
+
+			newUser := fakeUser()
+			newUser.Username = user.Username
+			s.Require().Error(s.model.Insert(newUser), "Should have thrown error")
+		})
+
+		s.Run("Existed account_name", func() {
+
+			newUser := fakeUser()
+			newUser.AccountName = user.AccountName
+			s.Require().Error(s.model.Insert(newUser), "Should have thrown error")
+		})
+
+		s.Run("Existed email", func() {
+
+			newUser := fakeUser()
+			newUser.Email = user.Email
+			s.Require().Error(s.model.Insert(newUser), "Should have thrown error")
+		})
+	})
+
+	s.Run("Should throw error when insert invalid user data", func() {
+
+		user := fakeUser()
+
+		s.Run("Use empty user ID", func() {
+
+			invalidUser := user
+			invalidUser.UserID = ""
+
+			s.Require().Error(s.model.Insert(invalidUser), "Should throw error")
+		})
+
+		s.Run("Use empty username", func() {
+
+			invalidUser := user
+			invalidUser.Username = ""
+
+			s.Require().Error(s.model.Insert(invalidUser), "Should throw error")
+		})
+
+		s.Run("Use empty password", func() {
+
+			invalidUser := user
+			invalidUser.Password = ""
+
+			s.Require().Error(s.model.Insert(invalidUser), "Should throw error")
+		})
+
+		s.Run("Use empty account name", func() {
+
+			invalidUser := user
+			invalidUser.AccountName = ""
+
+			s.Require().Error(s.model.Insert(invalidUser), "Should throw error")
+		})
+
+		s.Run("Use empty email", func() {
+
+			invalidUser := user
+			invalidUser.Email = ""
+
+			s.Require().Error(s.model.Insert(invalidUser), "Should throw error")
+		})
+	})
+}
+
+// func (s *UsersModelTestSuite) TestGetByID() {
+
+// 	s.Run("Should get the user by user_id properly", func() {
+
+// 		actual, err := s.model.GetByID(s.insertedUser.UserID)
+// 		s.Require().NoError(err, "Getting exist user failed")
+// 		s.Require().EqualValues(s.insertedUser, actual)
+// 	})
+
+// 	s.Run("Should throw the error when give non-exist user_id", func() {
+
+// 		itemID := "non-exist_id"
+
+// 		actual, err := s.model.GetByID(itemID)
+// 		s.Require().Empty(actual)
+// 		s.Require().ErrorIs(errors.ObjectIDNotFoundError.New(itemID), err, "Should throw error")
+// 	})
+// }
+
+// func (s *UsersModelTestSuite) TestSearch() {
+
+// 	createPasswordFunc := func() string {
+// 		return gofakeit.Password(true, true, true, true, false, 10)
+// 	}
+
+// 	userA := objects.User{
+// 		UserID:      gofakeit.UUID(),
+// 		Username:    "username_a",
+// 		Password:    createPasswordFunc(),
+// 		AccountName: "User A",
+// 		Email:       "userA@example.com",
+// 	}
+
+// 	userB := objects.User{
+// 		UserID:      gofakeit.UUID(),
+// 		Username:    "username_b",
+// 		Password:    createPasswordFunc(),
+// 		AccountName: "User B",
+// 		Email:       "userB@example.com",
+// 	}
+
+// 	userC := objects.User{
+// 		UserID:      gofakeit.UUID(),
+// 		Username:    "username_c",
+// 		Password:    createPasswordFunc(),
+// 		AccountName: "User C",
+// 		Email:       "userC@example.com",
+// 	}
+
+// 	var initialLimit = s.model.SearchLenLimit
+// 	s.model.SearchLenLimit = 2
+// 	s.T().Cleanup(func() {
+// 		s.model.SearchLenLimit = initialLimit
+// 	})
+
+// 	users := []objects.User{userA, userB, userC}
+// 	gofakeit.ShuffleAnySlice(users)
+// 	for _, user := range users {
+// 		s.Require().NoError(s.model.Insert(user), "Insert users before testing failed")
+// 	}
+
+// 	slices.SortFunc(users, func(a, b objects.User) int {
+
+// 		if a.UserID > b.UserID {
+// 			return 1
+// 		}
+
+// 		return -1
+// 	})
+
+// 	s.Run("Should get user(s) properly by given options", func() {
+
+// 		var testCases = map[string]struct {
+// 			Expected models.PaginationData[objects.User]
+// 			Option   SearchOption
+// 		}{
+// 			"None (Page 1)": {
+// 				Expected: models.PaginationData[objects.User]{
+// 					Page:       1,
+// 					TotalPages: 2,
+// 					Data:       users[:2],
+// 				},
+// 				Option: SearchOption{
+// 					CurrentPage: 1,
+// 				},
+// 			},
+// 			"None (Page 2)": {
+// 				Expected: models.PaginationData[objects.User]{
+// 					Page:       2,
+// 					TotalPages: 2,
+// 					Data:       users[2:],
+// 				},
+// 				Option: SearchOption{
+// 					CurrentPage: 2,
+// 				},
+// 			},
+// 			"User ID (Equal)": {
+// 				Expected: models.PaginationData[objects.User]{
+// 					Page:       1,
+// 					TotalPages: 1,
+// 					Data:       []objects.User{userA},
+// 				},
+// 				Option: SearchOption{
+// 					CurrentPage: 1,
+// 					UserID:      userA.UserID,
+// 				},
+// 			},
+// 			"Username (Equal)": {
+// 				Expected: models.PaginationData[objects.User]{
+// 					Page:       1,
+// 					TotalPages: 1,
+// 					Data:       []objects.User{userB},
+// 				},
+// 				Option: SearchOption{
+// 					CurrentPage: 1,
+// 					Username: models.MatchOption{
+// 						MatchType: models.EqualMatchType,
+// 						Value:     userB.Username,
+// 					},
+// 				},
+// 			},
+// 			"Username (Partial)": {
+// 				Expected: models.PaginationData[objects.User]{
+// 					Page:       1,
+// 					TotalPages: 2,
+// 					Data:       users[:2],
+// 				},
+// 				Option: SearchOption{
+// 					CurrentPage: 1,
+// 					Username: models.MatchOption{
+// 						MatchType: models.PartialMatchType,
+// 						Value:     "er",
+// 					},
+// 				},
+// 			},
+// 			"Username (Start with)": {
+// 				Expected: models.PaginationData[objects.User]{
+// 					Page:       1,
+// 					TotalPages: 2,
+// 					Data:       users[:2],
+// 				},
+// 				Option: SearchOption{
+// 					CurrentPage: 1,
+// 					Username: models.MatchOption{
+// 						MatchType: models.StartWithMatchType,
+// 						Value:     "us",
+// 					},
+// 				},
+// 			},
+// 			"Username (End with)": {
+// 				Expected: models.PaginationData[objects.User]{
+// 					Page:       1,
+// 					TotalPages: 1,
+// 					Data:       []objects.User{userC},
+// 				},
+// 				Option: SearchOption{
+// 					CurrentPage: 1,
+// 					Username: models.MatchOption{
+// 						MatchType: models.EndWithMatchType,
+// 						Value:     "c",
+// 					},
+// 				},
+// 			},
+// 			// "Account name (Equal)": {
+// 			// 	Expected: models.PaginationData[objects.User]{
+// 			// 		Page:       1,
+// 			// 		TotalPages: 1,
+// 			// 		Data:       []objects.User{userB},
+// 			// 	},
+// 			// 	Option: SearchOption{
+// 			// 		CurrentPage: 1,
+// 			// 		AccountName: models.MatchOption{
+// 			// 			MatchType: models.EqualMatchType,
+// 			// 			Value:     userB.AccountName,
+// 			// 		},
+// 			// 	},
+// 			// },
+// 			// "Account name (Partial)": {
+// 			// 	Expected: models.PaginationData[objects.User]{
+// 			// 		Page:       1,
+// 			// 		TotalPages: 2,
+// 			// 		Data:       users[:2],
+// 			// 	},
+// 			// 	Option: SearchOption{
+// 			// 		CurrentPage: 1,
+// 			// 		AccountName: models.MatchOption{
+// 			// 			MatchType: models.PartialMatchType,
+// 			// 			Value:     "user",
+// 			// 		},
+// 			// 	},
+// 			// },
+// 			// "Account name (Start with)": {
+// 			// 	Expected: models.PaginationData[objects.User]{
+// 			// 		Page:       1,
+// 			// 		TotalPages: 2,
+// 			// 		Data:       users[:2],
+// 			// 	},
+// 			// 	Option: SearchOption{
+// 			// 		CurrentPage: 1,
+// 			// 		AccountName: models.MatchOption{
+// 			// 			MatchType: models.StartWithMatchType,
+// 			// 			Value:     "use",
+// 			// 		},
+// 			// 	},
+// 			// },
+// 			// "Account name (End with)": {
+// 			// 	Expected: models.PaginationData[objects.User]{
+// 			// 		Page:       1,
+// 			// 		TotalPages: 1,
+// 			// 		Data:       []objects.User{userA},
+// 			// 	},
+// 			// 	Option: SearchOption{
+// 			// 		CurrentPage: 1,
+// 			// 		AccountName: models.MatchOption{
+// 			// 			MatchType: models.EndWithMatchType,
+// 			// 			Value:     "a",
+// 			// 		},
+// 			// 	},
+// 			// },
+// 			// "Email (Equal)": {
+// 			// 	Expected: models.PaginationData[objects.User]{
+// 			// 		Page:       1,
+// 			// 		TotalPages: 1,
+// 			// 		Data:       []objects.User{userB},
+// 			// 	},
+// 			// 	Option: SearchOption{
+// 			// 		CurrentPage: 1,
+// 			// 		Email: models.MatchOption{
+// 			// 			MatchType: models.EqualMatchType,
+// 			// 			Value:     userB.Email,
+// 			// 		},
+// 			// 	},
+// 			// },
+// 			// "Email (Partial)": {
+// 			// 	Expected: models.PaginationData[objects.User]{
+// 			// 		Page:       1,
+// 			// 		TotalPages: 2,
+// 			// 		Data:       users[:2],
+// 			// 	},
+// 			// 	Option: SearchOption{
+// 			// 		CurrentPage: 1,
+// 			// 		Email: models.MatchOption{
+// 			// 			MatchType: models.PartialMatchType,
+// 			// 			Value:     "@",
+// 			// 		},
+// 			// 	},
+// 			// },
+// 			// "Email (Start with)": {
+// 			// 	Expected: models.PaginationData[objects.User]{
+// 			// 		Page:       1,
+// 			// 		TotalPages: 2,
+// 			// 		Data:       users[:2],
+// 			// 	},
+// 			// 	Option: SearchOption{
+// 			// 		CurrentPage: 1,
+// 			// 		Email: models.MatchOption{
+// 			// 			MatchType: models.StartWithMatchType,
+// 			// 			Value:     "use",
+// 			// 		},
+// 			// 	},
+// 			// },
+// 			// "Email (End with)": {
+// 			// 	Expected: models.PaginationData[objects.User]{
+// 			// 		Page:       1,
+// 			// 		TotalPages: 2,
+// 			// 		Data:       users[:2],
+// 			// 	},
+// 			// 	Option: SearchOption{
+// 			// 		CurrentPage: 1,
+// 			// 		Email: models.MatchOption{
+// 			// 			MatchType: models.EndWithMatchType,
+// 			// 			Value:     "m",
+// 			// 		},
+// 			// 	},
+// 			// },
+// 		}
+
+// 		for optionName, testCase := range testCases {
+
+// 			s.Run(fmt.Sprintf("Search with option %s", optionName), func() {
+
+// 				fmt.Println(optionName)
+
+// 				paginationData, err := s.model.Search(testCase.Option)
+// 				s.Require().NoError(err, "Searching user failed")
+// 				s.Require().EqualValues(testCase.Expected, paginationData)
+// 			})
+// 		}
+// 	})
+
+// 	s.Run("Should throw error when set current page as non-positive value", func() {
+
+// 		result, err := s.model.Search(SearchOption{CurrentPage: 0})
+// 		s.Require().ErrorIs(errors.CurrentPageInvalidError.New(), err, "Should have returned error")
+// 		s.Require().Empty(result)
+// 	})
+
+// 	s.Run("Should throw error when set invalid or unsupported match type", func() {
+
+// 		result, err := s.model.Search(
+// 			SearchOption{
+// 				CurrentPage: 1,
+// 				Username: models.MatchOption{
+// 					MatchType: 255,
+// 				},
+// 			},
+// 		)
+// 		s.Require().Error(err, "Should have returned error")
+// 		s.Require().Empty(result)
+// 	})
+
+// }
+
+// func (s *UsersModelTestSuite) TestUpdate() {
+
+// 	s.Run("Should update exist user properly", func() {
+
+// 		userToUpdate := fakeUser()
+// 		userToUpdate.UserID = s.insertedUser.UserID
+
+// 		s.Require().NoError(s.model.Update(userToUpdate))
+
+// 		s.T().Cleanup(func() {
+// 			s.Require().NoError(s.model.Update(s.insertedUser))
+// 		})
+
+// 		actual, err := s.model.GetByID(userToUpdate.UserID)
+// 		s.Require().NoError(err, "Getting updated user failed")
+// 		s.Require().EqualValues(userToUpdate, actual)
+// 	})
+
+// 	s.Run("Should update partial data in user properly", func() {
+
+// 		mockUser := fakeUser()
+
+// 		var userToUpdate = objects.User{
+// 			UserID:   s.insertedUser.UserID,
+// 			Password: mockUser.Password,
+// 			Email:    mockUser.Email,
+// 		}
+
+// 		s.Require().NoError(s.model.Update(userToUpdate))
+
+// 		s.T().Cleanup(func() {
+// 			s.Require().NoError(s.model.Update(s.insertedUser))
+// 		})
+
+// 		expected := s.insertedUser
+// 		expected.Password = userToUpdate.Password
+// 		expected.Email = userToUpdate.Email
+
+// 		actual, err := s.model.GetByID(expected.UserID)
+// 		s.Require().NoError(err, "Getting updated user failed")
+// 		s.Require().EqualValues(expected, actual)
+// 	})
+
+// 	s.Run("Should throw error when update non-exist user", func() {
+
+// 		userToUpdate := fakeUser()
+// 		s.Require().Error(s.model.Update(userToUpdate))
+
+// 		actual, err := s.model.GetByID(s.insertedUser.UserID)
+// 		s.Require().NoError(err, "Getting updated user failed")
+// 		s.Require().EqualValues(s.insertedUser, actual)
+// 	})
+// }
+
+// func (s *UsersModelTestSuite) TestDelete() {
+
+// 	s.Run("Should delete exist user properly", func() {
+
+// 		s.Require().NoError(s.model.Delete(s.insertedUser.UserID))
+
+// 		actual, err := s.model.GetByID(s.insertedUser.UserID)
+// 		s.Require().Error(err, "Should throw error after getting deleted user")
+// 		s.Require().Empty(actual, "The user should have been empty")
+// 	})
+
+// 	s.Run("Should throw error when delete non-exist user", func() {
+
+// 		s.Require().Error(s.model.Delete("invalid_user_id"), "Delete exist user failed")
+// 	})
+// }
+
+func TestUsersModel(t *testing.T) {
+
+	suite.Run(t, new(UsersModelTestSuite))
+}
+
+func fakeUser() objects.User {
+
+	return objects.User{
+		UserID:      gofakeit.UUID(),
+		Username:    gofakeit.Username(),
+		Password:    gofakeit.Password(true, true, true, true, false, 10),
+		AccountName: gofakeit.FirstName(),
+		Email:       gofakeit.Email(),
+	}
+}
